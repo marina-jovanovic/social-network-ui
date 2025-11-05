fa80b8e5d378fc87d955a86ea84d36a678ce627a
Marina Jovanovic <marinajovanovic95@gmail.com>
2025-11-05 21:36:38 +0100

    feat: Implementirana funkcionalnost za prikaz korisnika odredjene grupe.

--- js/users.js
+++ js/users.js
@@ -22,5 +22,39 @@ function renderUsers(users) {
   });
 }
 
-
 window.addEventListener("DOMContentLoaded", fetchUsers);
+
+
+function fetchGroupUsers(groupId) {
+  fetch(`http://localhost:5171/users/group/${groupId}`)
+    .then(response => {
+      if (!response.ok) throw new Error("Nema korisnika u grupi");
+      return response.json();
+    })
+    .then(users => renderGroupUsers(users))
+    .catch(err => console.error(err));
+}
+
+function renderGroupUsers(users) {
+  const tbody = document.getElementById("groupUserList");
+  tbody.innerHTML = "";
+  users.forEach(user => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${user.id}</td>
+      <td>${user.name}</td>
+      <td>${user.surname}</td>
+      <td>${user.dateOfBirth}</td>
+    `;
+    tbody.appendChild(tr);
+  });
+}
+
+document.getElementById("groupId").addEventListener("change", (e) => {
+  fetchGroupUsers(e.target.value);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const select = document.getElementById("groupId");
+  fetchGroupUsers(select.value);
+});
